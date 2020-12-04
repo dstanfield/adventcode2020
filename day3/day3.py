abe748897ee3fe6cdc24e78116d5b9efee817436
dstanfield <dstanfieldva@gmail.com>
2020-12-03 22:33:22 -0500

    yikes that is ugly

--- day3/day3.py
+++ day3/day3.py
@@ -1,13 +1,19 @@
 # import as list of lists 
-# keep record of: overall x and y position, relative x and y position, number of hashes
+# keep record of: overall position and y position, relative position and y position, number of hashes
 
 import sys
 
 def main():
     
     coordList = load_file("input.txt")
-    numTrees = move(coordList)
-    print(numTrees)
+    numTrees1 = move(coordList, 1, 1)
+    numTrees2 = move(coordList, 3, 1)
+    numTrees3 = move(coordList, 5, 1)
+    numTrees4 = move(coordList, 7, 1)
+    numTrees5 = move(coordList, 1, 2)
+    numMult = numTrees1 * numTrees2 * numTrees3 * numTrees4 * numTrees5
+    print(numMult)
+    print(numTrees2)
 
 
 def load_file(file):
@@ -17,14 +23,20 @@ def load_file(file):
     except IOError:
         sys.exit("ERROR: Cannot load file: %s" % file)
 
-def move(coords):
-    x = 0
+def move(coords, right, down):
+    position = 0
     trees = 0
+    placekeeper = 1
     for points in coords:
-        wrap = x%len(points)
+        wrap = position%len(points)
+        if down == 2:
+            placekeeper +=1
+            if placekeeper%2 == 1:
+                continue
         if points[wrap] == "#":
             trees +=1
-        x +=3
+        
+        position +=right
     return trees
 
 
